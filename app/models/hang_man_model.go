ddcbd977874ccc0fa606fd38e28d46ccc14b6990
Jeff Rossi <rossi.jeff@gmail.com>
2023-07-27 20:30:31 -0400

    hangman, klondike, poker square

---
+++ app/models/hang_man_model.go
@@ -0,0 +1,14 @@
+package models
+
+import "go-games-api/app/enum"
+
+type HangMan struct {
+	BaseModel
+	Correct, Wrong string
+	Status         enum.GameStatus
+	Score          int
+	UserId         int64 `json:"user_id"`
+	User           User  `json:"user,omitempty"`
+	WordId         int64 `json:"word_id"`
+	Word           Word  `json:"word,omitempty"`
+}
